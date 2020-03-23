eb47d061e59f99cc4e12264077bad8b2e36350b3
AustinPotts <pottsevolvements@gmail.com>
2020-03-23 12:09:16 -0400

    Configured Contact Class

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
@@ -0,0 +1,23 @@
+//
+//  Contact.swift
+//  Contacts-Hybrid
+//
+//  Created by Austin Potts on 3/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+class Contact {
+    
+    
+    var name: String
+    var relationship: String?
+    
+    init(name: String, relationship: String){
+        self.name = name
+        self.relationship = relationship
+    }
+    
+    
+}
